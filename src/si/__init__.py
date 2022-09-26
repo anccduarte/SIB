93745b4c9a35bc6acb7a9aaa011f2caed0f072e6
Nuno Duarte <antonionuno.123@gmail.com>
2022-09-26 16:32:30 +0100

    Adding my co-authorship to README.md file

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,6 +1,6 @@
-__author__ = "Fernando Cruz"
+__author__ = "António Duarte"
 __credits__ = ["Vítor Pereira, Fernando Cruz"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Fernando Cruz"
-__email__ = "fernando.cruz@ceb.uminho.pt"
+__maintainer__ = "António Duarte"
+__email__ = "pg45464@alunos.uminho.pt"

70c94025d219d779b1247c1902c674be945e2412
anccduarte <antonionuno.123@gmail.com>
2022-09-27 18:42:29 +0100

    [ADD] Added type checking and NaN handling

--- src/si/data/dataset.py
+++ src/si/data/dataset.py
@@ -8,14 +8,21 @@ class Dataset:
 		"""
 		Inicializa uma instância da classe Dataset.
 		"""
+		if type(X) is not np.ndarray:
+			raise TypeError("O parâmetro 'X' deve ser do tipo 'np.ndarray'.")
+		if type(y) is not np.ndarray and y is not None:
+			raise TypeError("O parâmetro 'y' deve ser do tipo 'np.ndarray' ou 'None'.")
+		if type(features) not in (list, tuple) and features is not None:
+			raise TypeError("O parâmetro 'features' deve ser do tipo 'list', 'tuple' ou 'None'.")
+		if type(label) is not str and label is not  None:
+			raise TypeError("O parâmetro 'label' deve ser do tipo 'str' ou 'None'.")
 		if y is not None:
 			if X.shape[0] != y.size:
-				msg = "O número de exemplos de 'X' deve ser igual à dimensão de 'y'."
-				raise ValueError(msg)
+				raise ValueError("O número de exemplos de 'X' deve ser igual à dimensão de 'y'.")
 		self.X = X
 		self.y = y
-		self.features = features
-		self.label = label
+		self.features = [f"feat{i+1}" for i in range(X.shape[1])] if features is None else features
+		self.label = "label" if label is None else label
 
 	def shape(self):
 		"""
@@ -68,8 +75,8 @@ class Dataset:
 
 	def summary(self):
 		"""
-		Retorna um pd.Dataframe contendo métricas descritivas (média, variância,
-		mediana, mínimo e máximo) de cada coluna do dataset.
+		Retorna um pd.Dataframe contendo métricas descritivas (média, variância, mediana, mínimo
+		e máximo) de cada coluna do dataset.
 		"""
 		df = pd.DataFrame({
 			"Mean": self.get_mean(),
@@ -80,19 +87,48 @@ class Dataset:
 			})
 		return df
 
+	def remove_nan(self):
+		"""
+		Remove linhas do dataset que contenham valores omissos (NaN).
+		"""
+		idx = np.isnan(self.X).any(axis=1)
+		self.X, self.y = self.X[~idx], self.y[~idx]
+
+	def fill_nan(self, fill):
+		"""
+		Subtitui todos os valores omissos (NaN) do dataset pela média/mediana da respetiva coluna.
+		"""
+		vals = {"mean": np.nanmean, "median": np.nanmedian}
+		self.X = np.nan_to_num(self.X, nan=vals[fill](self.X,axis=0))
+
 if __name__ == "__main__":
-	x1 = np.array([[1,2,3],[4,5,6]])
+
+	print("EX1")
+	X1 = np.array([[1,2,3],[4,5,6]])
 	y1 = np.array([1,2])
 	f1 = ["A","B"]
 	l1 = "y"
-	ds1 = Dataset(x1, y1, f1, l1)
+	ds1 = Dataset(X1, y1, f1, l1)
 	print(f"shape: {ds1.shape()}")
 	print(f"has_label: {ds1.has_label()}")
 	print(f"classes: {ds1.get_classes()}")
-	#print(f"means: {ds1.get_mean()}")
-	#print(f"variances: {ds1.get_variance()}")
-	#print(f"medians: {ds1.get_median()}")
-	#print(f"mins: {ds1.get_min()}")
-	#print(f"maxs: {ds1.get_max()}")
 	print(ds1.summary())
+
+	print("\nEX2")
+	X2 = np.array([[1,2,3],[1,np.nan,np.nan],[4,5,6]])
+	y2 = np.array([7,8,9])
+	ds2 = Dataset(X2, y2)
+	print(f"shape (before removing NaNs): {ds2.shape()}")
+	ds2.remove_nan()
+	print(f"shape (after removing NaNs): {ds2.shape()}")
+	print(ds2.summary())
+
+	print("\nEX3")
+	X3 = np.array([[1,2,3],[1,np.nan,np.nan],[4,5,6]])
+	y3 = np.array([7,8,9])
+	ds3 = Dataset(X3, y3)
+	print(f"shape (before filling NaNs): {ds3.shape()}")
+	ds3.fill_nan("mean")
+	print(f"shape (after filling NaNs): {ds3.shape()}")
+	print(ds3.summary())
 

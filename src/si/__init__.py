7fb8fff4e3c6d2af74a851901e7b47ef1c537d26
anccduarte <antonionuno.123@gmail.com>
2022-10-17 11:50:33 +0100

    Updated README and __init__

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,6 +1,8 @@
 __author__ = "António Duarte"
-__credits__ = ["Vítor Pereira, Fernando Cruz"]
+__credits__ = "António Duarte"
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "António Duarte"
 __email__ = "pg45464@alunos.uminho.pt"
+
+# This package is heavily inspired and adapted from https://github.com/vmspereira/si.

b4f8219be79f058b3c17bac0a051f1e9647e238e
anccduarte <antonionuno.123@gmail.com>
2022-11-08 16:58:19 +0000

    Updated code (less repetition)

--- exercises/regression_cost_graphs.py
+++ exercises/regression_cost_graphs.py
@@ -13,53 +13,42 @@ from split import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
-# Ridge Regression (cpu)
+# ridge regression (cpu)
 cpu_file = "../datasets/cpu/cpu.csv"
 cpu = read_csv_file(file=cpu_file, sep=",", features=True, label=True)
 cpu.X = StandardScaler().fit_transform(cpu.X)
 cpu_trn, cpu_tst = train_test_split(cpu, test_size=0.3, random_state=2)
 
-# Without adaptative alpha
-cpu_ridge_1 = RidgeRegression(l2_penalty=1, alpha=0.001, max_iter=2000, tolerance=1, adaptative_alpha=False).fit(cpu_trn)
-predictions_1 = cpu_ridge_1.predict(cpu_tst)
-print("Predictions (cpu ridge regression without adaptative alpha)\n" + str(predictions_1))
-cost_dict_1 = cpu_ridge_1.cost_history
-plt.plot(list(cost_dict_1.keys()), list(cost_dict_1.values()), linestyle="-", color="red")
-plt.title("Ridge Regression without adaptative alpha"), plt.xlabel("Iteration"), plt.ylabel("Cost")
-plt.show()
 
-# With adaptative alpha
-cpu_ridge_2 = RidgeRegression(l2_penalty=1, alpha=0.001, max_iter=2000, tolerance=1, adaptative_alpha=True).fit(cpu_trn)
-predictions_2 = cpu_ridge_2.predict(cpu_tst)
-print("\nPredictions (cpu ridge regression without adaptative alpha)\n" + str(predictions_2))
-cost_dict_2 = cpu_ridge_2.cost_history
-plt.plot(list(cost_dict_2.keys()), list(cost_dict_2.values()), linestyle="-", color="blue")
-plt.title("Ridge Regression with adaptative alpha"), plt.xlabel("Iteration"), plt.ylabel("Cost")
-plt.show()
-
-############################################################################################################################################
-
-# Logistic Regression (breast)
+# logistic regression (breast)
 breast_file = "../datasets/breast/breast-bin.csv"
 breast = read_csv_file(file=breast_file, sep=",", features=False, label=True)
 breast.X = StandardScaler().fit_transform(breast.X)
 breast_trn, breast_tst = train_test_split(breast, test_size=0.3, random_state=2)
 
-# Without adaptative alpha
-breast_log_1 = LogisticRegression(l2_penalty=1, alpha=0.001, max_iter=2000, tolerance=0.0001, adaptative_alpha=False).fit(breast_trn)
-predictions_1 = breast_log_1.predict(breast_tst)
-print("\nPredictions (breast logistic regression without adaptative alpha)\n" + str(predictions_1))
-cost_dict_1 = breast_log_1.cost_history
-plt.plot(list(cost_dict_1.keys()), list(cost_dict_1.values()), linestyle="-", color="red")
-plt.title("Logistic Regression without adaptative alpha"), plt.xlabel("Iteration"), plt.ylabel("Cost")
-plt.show()
 
-# With adaptative alpha
-breast_log_2 = LogisticRegression(l2_penalty=1, alpha=0.001, max_iter=2000, tolerance=0.0001, adaptative_alpha=True).fit(breast_trn)
-predictions_2 = breast_log_2.predict(breast_tst)
-print("\nPredictions (breast logistic regression without adaptative alpha)\n" + str(predictions_2))
-cost_dict_2 = breast_log_2.cost_history
-plt.plot(list(cost_dict_2.keys()), list(cost_dict_2.values()), linestyle="-", color="blue")
-plt.title("Logistic Regression with adaptative alpha"), plt.xlabel("Iteration"), plt.ylabel("Cost")
-plt.show()
+# define variables
+n_graphs = 4
+datasets = {"cpu": (cpu_trn, cpu_tst), "breast": (breast_trn, breast_tst)}
+models = {"cpu": RidgeRegression, "breast": LogisticRegression}
+titles = {0: "Ridge Regression without adaptative alpha", 1: "Ridge Regression with adaptative alpha",
+          2: "Logistic Regression without adaptative alpha", 3: "Logistic Regression with adaptative alpha"}
+l2_penalty = 1
+alpha = 0.001
+max_iter = 2000
+tolerance = (1, 1, 0.0001, 0.0001)
+adaptative_alpha = (False, True, False, True)
+color = ("red", "blue", "red", "blue")
+
+# main loop -> print predictions and draw graphs for every combination model-alpha
+for i in range(n_graphs):
+    dataset = "cpu" if i < 2 else "breast"
+    model = models[dataset](l2_penalty, alpha, max_iter, tolerance[i], adaptative_alpha[i])
+    model.fit(datasets[dataset][0])
+    preds = model.predict(datasets[dataset][1])
+    print(f"Predictions ({titles[i]})\n{preds}\n")
+    cost_dict = model.cost_history
+    plt.plot(list(cost_dict.keys()), list(cost_dict.values()), linestyle="-", color=color[i])
+    plt.title(titles[i]), plt.xlabel("Iteration"), plt.ylabel("Cost")
+    plt.show()
 

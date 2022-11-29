53dd42c6821006df0f2b1cb432f05c15e2b19896
anccduarte <antonionuno.123@gmail.com>
2022-11-29 15:33:25 +0000

    Added mse_derivative to mse.py

--- src/si/metrics/mse.py
+++ src/si/metrics/mse.py
@@ -12,18 +12,32 @@ def mse(y_true: np.ndarray, y_pred: np.ndarray) -> float:
         The true labels of the dataset
     y_pred: np.ndarray
         The predicted labels of the dataset
-
-    Returns
-    -------
-    mse: float
-        The mean squared error of the model
     """
     N = y_true.shape[0]
-    return np.sum((y_true-y_pred)**2) / (2*N)
+    return np.sum((y_true - y_pred) ** 2) / (2*N)
+
+def mse_derivative(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    Computes and returns the value of the partial derivative of the Mean-Squared
+    Error (MSE) with respect to y_pred. d(MSE)/d(pi): -SUM[(ti - pi)] / N.
+
+    Parameters
+    ----------
+    y_true: np.ndarray
+        The true labels of the dataset
+    y_pred: np.ndarray
+        The predicted labels of the dataset
+    """
+    N = y_true.shape[0]
+    return -np.sum(y_true - y_pred) / N
 
 
 if __name__ == "__main__":
+
     true = np.array([0,1,1,1,0,1])
     pred = np.array([1,0,1,1,0,1])
+    # MSE
     print(f"MSE: {mse(true, pred):.4f}")
+    # MSE derivative with respect to y_pred
+    print(f"d(MSE)/d(y_pred): {mse_derivative(true, pred):.4f}")
 

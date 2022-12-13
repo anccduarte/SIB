b52247b6a454f6b10adbeaaf4125498be30dbcb6
anccduarte <antonionuno.123@gmail.com>
2022-12-13 17:47:55 +0000

    Added derivatives of activation functions

--- src/si/neural_networks/activation.py
+++ src/si/neural_networks/activation.py
@@ -1,6 +1,9 @@
 
 import numpy as np
 
+
+# -- ACTIVATION FUNCTIONS
+
 def identity(X: np.ndarray) -> np.ndarray:
     """
     Implements the identity activation function (the input array remains
@@ -13,6 +16,18 @@ def identity(X: np.ndarray) -> np.ndarray:
     """
     return X
 
+def relu(X: np.ndarray) -> np.ndarray:
+    """
+    Implements the ReLU (rectified linear unit) activation function. It is
+    defined as f(X) = max(0, X).
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The array of values to be activated
+    """
+    return np.maximum(0, X)
+
 def sigmoid(X: np.ndarray) -> np.ndarray:
     """
     Implements the sigmoid activation function. It is defined as follows:
@@ -24,20 +39,6 @@ def sigmoid(X: np.ndarray) -> np.ndarray:
         The array of values to be activated
     """
     return 1 / (1 + np.exp(-X))
-
-def tanh(X: np.ndarray) -> np.ndarray:
-    """
-    Implements the TanH (hyperbolic tangent) activation function. It is
-    defined as f(X) = (e^X - e^(-X)) / (e^X + e^(-X)).
-
-    Parameters
-    ----------
-    X: np.ndarray
-        The array of values to be activated
-    """
-    num = np.exp(X) - np.exp(-X)
-    denom = np.exp(X) + np.exp(-X)
-    return num / denom
 
 def softmax(X: np.ndarray) -> np.ndarray:
     """
@@ -54,17 +55,78 @@ def softmax(X: np.ndarray) -> np.ndarray:
     z_sum = np.sum(z_exp)
     return z_exp / z_sum
 
-def relu(X: np.ndarray) -> np.ndarray:
+def tanh(X: np.ndarray) -> np.ndarray:
     """
-    Implements the ReLU (rectified linear unit) activation function. It is
-    defined as f(X) = max(0, X).
+    Implements the TanH (hyperbolic tangent) activation function. It is
+    defined as f(X) = (e^X - e^(-X)) / (e^X + e^(-X)).
 
     Parameters
     ----------
     X: np.ndarray
         The array of values to be activated
     """
-    return np.maximum(0, X)
+    num = np.exp(X) - np.exp(-X)
+    denom = np.exp(X) + np.exp(-X)
+    return num / denom
+
+
+# -- ACTIVATION DERIVATIVES
+
+def d_identity(X: np.ndarray) -> np.ndarray:
+    """
+    Computes the derivative of the identity activation function.
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The array of values to which the derivative is applied
+    """
+    return np.ones(X.size).reshape(X.shape)
+
+def d_relu(X: np.ndarray) -> np.ndarray:
+    """
+    Computes the derivative of the ReLU (rectified linear unit) activation function.
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The array of values to which the derivative is applied
+    """
+    return np.heaviside(X, 1)
+
+def d_sigmoid(X: np.ndarray) -> np.ndarray:
+    """
+    Computes the derivative of the sigmoid activation function.
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The array of values to which the derivative is applied
+    """
+    return sigmoid(X) * (1 - sigmoid(X))
+
+def d_softmax(X: np.ndarray) -> np.ndarray:
+    """
+    Computes the derivative of the softmax activation function.
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The array of values to which the derivative is applied
+    """
+    # TODO
+    return np.ones(X.size).reshape(X.shape)
+
+def d_tanh(X: np.ndarray) -> np.ndarray:
+    """
+    Computes the derivative of the TanH (hyperbolic tangent) activation function.
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The array of values to which the derivative is applied
+    """
+    return 1 - (tanh(X) ** 2)
 
 
 if __name__ == "__main__":

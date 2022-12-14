1ba692fd12b02d67bf97cfa3117906d44299c8eb
anccduarte <antonionuno.123@gmail.com>
2022-12-14 17:57:23 +0000

    Discerned between binary and categorical cross-entropy

--- src/si/metrics/cross_entropy.py
+++ src/si/metrics/cross_entropy.py
@@ -1,10 +1,13 @@
 
 import numpy as np
 
-def cross_entropy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+
+# -- BINARY
+
+def binary_cross_entropy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     """
-    Computes and returns the value of the Cross-Entropy Loss of a given model.
-    Cross-Entropy: -SUM[ti - ln(pi)] / N.
+    Computes and returns the value of the binary Cross-Entropy Loss of a given model.
+    Binary Cross-Entropy: -SUM[yt * log(yp) + (1 - yt) * log(1 - yp)] / N.
 
     Parameters
     ----------
@@ -14,12 +17,12 @@ def cross_entropy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
         The predicted labels of the dataset
     """
     N = y_true.shape[0]
-    return -np.sum(y_true * np.log(y_pred)) / N
+    return -np.sum((y_true * np.log(y_pred)) + (1 - y_true) + np.log(1 - y_pred)) / N
 
-def cross_entropy_derivative(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+def d_binary_cross_entropy(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
     """
-    Computes and returns the value of the partial derivative of the Cross-Entropy
-    Loss with respect to y_pred. d(Cross-Entropy)/d(pi): SUM[1/pi] / N.
+    Computes and returns the value of the partial derivative of the binary Cross-Entropy
+    Loss with respect to y_pred.
 
     Parameters
     ----------
@@ -29,16 +32,46 @@ def cross_entropy_derivative(y_true: np.ndarray, y_pred: np.ndarray) -> float:
         The predicted labels of the dataset
     """
     N = y_true.shape[0]
-    return -y_true / (y_pred * N)
+    return ((-y_true / y_pred) + ((1 - y_true) / (1 - y_pred))) / N
+
+
+# -- CATEGORICAL
+
+def categorical_cross_entropy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    Computes and returns the value of the categorical Cross-Entropy Loss of a given
+    model. Categorical Cross-Entropy: -SUM[ti - ln(pi)].
+
+    Parameters
+    ----------
+    y_true: np.ndarray
+        The true labels of the dataset
+    y_pred: np.ndarray
+        The predicted labels of the dataset
+    """
+    return -np.sum(y_true * np.log(y_pred))
+
+def d_categorical_cross_entropy(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    """
+    Computes and returns the value of the partial derivative of the categorical
+    Cross-Entropy Loss with respect to y_pred.
+
+    Parameters
+    ----------
+    y_true: np.ndarray
+        The true labels of the dataset
+    y_pred: np.ndarray
+        The predicted labels of the dataset
+    """
+    return y_pred - y_true
 
 
 if __name__ == "__main__":
 
-	# ERROR -> division by 0
     true = np.array([0,1,1,1,0,1])
-    pred = np.array([1,1,1,1,1,1])
+    pred = np.array([0.9,0.1,0.1,0.9,0.1,0.9])
     # Cross-Entropy
-    print(f"Cross-Entropy: {cross_entropy(true, pred):.4f}")
+    print(f"Cross-Entropy: {binary_cross_entropy(true, pred):.4f}")
     # Cross-Entropy derivative with respect to y_pred
-    print(f"d(Cross-Entropy)/d(y_pred): {cross_entropy_derivative(true, pred)}")
+    print(f"d(Cross-Entropy)/d(y_pred): {d_binary_cross_entropy(true, pred)}")
 

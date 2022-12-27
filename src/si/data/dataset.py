b35b004fc64470ae92027f6f9d7a2ccc4bdc13ca
anccduarte <antonionuno.123@gmail.com>
2022-12-27 11:37:10 +0000

    Added customizable feature and label ranges to cls.from_random

--- src/si/data/dataset.py
+++ src/si/data/dataset.py
@@ -6,17 +6,19 @@ from typing import Tuple
 class Dataset:
 
     """
-    Constructs a tabular dataset for machine learning.
+    Constructs a tabular dataset for machine learning. The dataset is built from a set of feature
+    vectors (X) and an optional label vector (y).
     """
 
     def __init__(self, X: np.ndarray, y: np.ndarray = None, features: list = None, label: str = None):
         """
-        Constructs a tabular dataset for machine learning.
+        Constructs a tabular dataset for machine learning. The dataset is built from a set of feature
+        vectors (X) and an optional label vector (y).
 
         Parameters
         ----------
         X: np.ndarray
-            The matrix containing the dataset's features
+            The matrix containing the dataset's feature vectors
         y: np.ndarray (default=None)
             The label vector
         features: list (deafult=None)
@@ -27,6 +29,7 @@ class Dataset:
         if y is not None:
             if X.shape[0] != y.size:
                 raise ValueError("The number of examples in 'X' must be equal to the size of 'y'.")
+        # reshape X if it only has one feature (otherwise, it defaults to a one dimensional array) 
         self.X = X if X.ndim > 1 else np.reshape(X, (-1, 1))
         self.y = y
         self.features = [f"feat{i+1}" for i in range(X.shape[1])] if features is None else features
@@ -42,7 +45,7 @@ class Dataset:
         """
         Returns a boolean value representative of the presence of a label.
         """
-        return True if self.y is not None else False
+        return self.y is not None
 
     def get_classes(self) -> np.ndarray:
         """
@@ -51,7 +54,6 @@ class Dataset:
         if self.y is None:
             raise ValueError("The parameter 'y' was set to 'None'.")
         return np.unique(self.y)
-        #return list(set(self.y))
 
     def get_mean(self) -> np.ndarray:
         """
@@ -88,21 +90,25 @@ class Dataset:
         Returns a pd.DataFrame containing some descriptive metrics (mean, variance, median,
         minimum value and maximum value) of each feature.
         """
-        df = pd.DataFrame({
-            "Mean": self.get_mean(),
-            "Variance": self.get_variance(),
-            "Median": self.get_median(),
-            "Min": self.get_min(),
-            "Max": self.get_max()
-            })
+        df = pd.DataFrame({"Mean": self.get_mean(),
+                           "Variance": self.get_variance(),
+                           "Median": self.get_median(),
+                           "Min": self.get_min(),
+                           "Max": self.get_max()})
         return df
 
     @classmethod
-    def from_random(cls, n_examples: int, n_features: int, label: bool, seed: int = None) -> 'Dataset':
+    def from_random(cls,
+                    n_examples: int,
+                    n_features: int,
+                    label: bool,
+                    features_range: tuple = (1, 10),
+                    label_range: tuple = (0, 3),
+                    seed: int = None) -> 'Dataset':
         """
-        Randomly generates and returns a new Dataset object based on given parameters.
-        The values of the dataset's features range from 1 to 9. If present, the values of
-        the dataset's label range from 0 to 2.
+        Randomly generates and returns a new Dataset object based on given parameters. By default,
+        the values of the dataset's features range from 1 to 9. If present, the values of the
+        dataset's label range, by deafult, from 0 to 2.
 
         Parameters
         ----------
@@ -112,11 +118,17 @@ class Dataset:
             Number of features in the dataset
         label: bool
             Whether the dataset has a label
-        seed: int
+        feature_range: tuple (default=(1, 10))
+            The interval of values used when generating the feature vectors
+        label_range: tuple (default=(0, 3))
+            The interval of values used when generating the label vector (ignored when label := None)
+        seed: int (default=None)
             Seed for the random number generator
         """
-        X = np.random.RandomState(seed=seed).randint(1, 10, (n_examples, n_features))
-        y = np.random.RandomState(seed=seed).randint(0, 3, (n_examples,)) if label else None
+        lf, uf = features_range
+        ll, ul = label_range
+        X = np.random.RandomState(seed=seed).randint(lf, uf, (n_examples, n_features))
+        y = np.random.RandomState(seed=seed).randint(ll, ul, (n_examples,)) if label else None
         return cls(X, y)
 
     def remove_nan(self):
@@ -141,7 +153,8 @@ class Dataset:
             The string description of the value by which missing values are replaced
         """
         vals = {"mean": np.nanmean, "median": np.nanmedian}
-        self.X = np.nan_to_num(self.X, nan=vals[fill](self.X,axis=0))
+        fill_func = vals[fill]
+        self.X = np.nan_to_num(self.X, nan=fill_func(self.X, axis=0))
 
 
 if __name__ == "__main__":
@@ -181,4 +194,16 @@ if __name__ == "__main__":
     print(f"has_label: {ds4.has_label()}")
     print(f"classes: {ds4.get_classes()}")
     print(ds4.summary())
+
+    print("\nEX5")
+    ds5 = Dataset.from_random(n_examples=20,
+                              n_features=6,
+                              label=True,
+                              features_range=(10, 20),
+                              label_range=(0, 2),
+                              seed=0)
+    print(f"shape: {ds5.shape()}")
+    print(f"has_label: {ds5.has_label()}")
+    print(f"classes: {ds5.get_classes()}")
+    print(ds5.summary())
 

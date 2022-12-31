Fixed the derivative of the softmax activation function

src/si/neural_networks/activation.py
@@ -1,5 +1,8 @@
 
 import numpy as np
+import sys
+sys.path.append("../statistics")
+from sigmoid_function import sigmoid_function
 
 
 # -- ACTIVATION FUNCTIONS
@@ -31,14 +34,14 @@ def relu(X: np.ndarray) -> np.ndarray:
 def sigmoid(X: np.ndarray) -> np.ndarray:
     """
     Implements the sigmoid activation function. It is defined as follows:
-    f(X) = 1 / (1 + e^(-X)).
+    f(X) = 1 / (1 + e^(-X)). Note: alias for sigmoid_function (statistics).
 
     Parameters
     ----------
     X: np.ndarray
         The array of values to be activated
     """
-    return 1 / (1 + np.exp(-X))
+    return sigmoid_function(X)
 
 def softmax(X: np.ndarray) -> np.ndarray:
     """
@@ -52,7 +55,7 @@ def softmax(X: np.ndarray) -> np.ndarray:
         The vector of values to be activated
     """
     z_exp = np.exp(X - np.amax(X))
-    z_sum = np.sum(z_exp)
+    z_sum = np.sum(z_exp, axis=1, keepdims=True)
     return z_exp / z_sum
 
 def tanh(X: np.ndarray) -> np.ndarray:
@@ -92,6 +95,7 @@ def d_relu(X: np.ndarray) -> np.ndarray:
     X: np.ndarray
         The array of values to which the derivative is applied
     """
+    # heaviside(x1, x2) = 0 if x1 < 0 else x2 if x1 == 0 else 1 (if x1 > 0)
     return np.heaviside(X, 1)
 
 def d_sigmoid(X: np.ndarray) -> np.ndarray:
@@ -107,15 +111,45 @@ def d_sigmoid(X: np.ndarray) -> np.ndarray:
 
 def d_softmax(X: np.ndarray) -> np.ndarray:
     """
-    Computes the derivative of the softmax activation function.
+    Computes the derivative of the softmax activation function. It assumes that
+    categorical cross-entropy is used as loss function. Combining the cross-entropy
+    and softmax gradients originates the formula (y_pred - y_true), which is
+    implemented in "../metrics/cross.entropy.py". Therefore, 'd_softmax' returns the
+    array it takes as input.
+
+    Formulas:
+    - softmax -> y_pred_i = exp(x_i) / SUM(k)[exp(x_k)]
+    - categorical cross-entropy -> L = -SUM(i)[y_true_i *ln(y_pred_i)]
+
+    Derivative of softmax:
+    - i = j
+      d(y_pred_i)/d(x_i) = 
+      = (exp(x_i) * SUM(k)[exp(x_k)] - exp(x_i)^2) / (SUM(k)[exp(x_k)])^2 =
+      = y_pred_i * (1 - y_pred_i)
+    - i != j
+      d(y_pred_i)/d(x_j) =
+      = (0 - exp(x_i) * exp(x_j)) / (SUM(k)[exp(x_k)])^2 =
+      = - y_pred_i * y_pred_j
+
+    Derivative of categorical cross-entropy:
+    - d(L)/d(y_pred_i) = -SUM(i)[y_true_i - (1 / y_pred_i)]
+
+    Combining gradients:
+    - d(L)/d(x_j) =
+      = - (SUM(i!=j)[y_true_i - (1 / y_pred_i)] * d(y_pred_i)/d(x_j) +
+        + y_true_j * (1 / y_pred_j) * d(y_pred_j)/d(x_j)) =
+      = - (SUM(i!=j)[y_true_i - (1 / y_pred_i)] * (-y_pred_i * y_pred_j) +
+        + y_true_j * (1 / y_pred_j) * y_pred_j * (1 - y_pred_j)) =
+      = SUM(i!=j)[y_true_i * y_pred_j] + y_true_j * y_pred_j - y_true_j =
+      = SUM(i)[y_true_i * y_pred_j] - y_true_j =
+      = y_pred_j - y_true_i
 
     Parameters
     ----------
     X: np.ndarray
         The array of values to which the derivative is applied
     """
-    # TODO
-    return np.ones(X.size).reshape(X.shape)
+    return X
 
 def d_tanh(X: np.ndarray) -> np.ndarray:
     """
@@ -131,9 +165,15 @@ def d_tanh(X: np.ndarray) -> np.ndarray:
 
 if __name__ == "__main__":
 
-    X = np.array([5,10,0,-4,-6,1,2,6,8])
+    X = np.array([[5,10,0,-4,-6,1,2,6,8]])
 
+    print("ACTIVATION FUNCTIONS")
     for a_func in [identity, sigmoid, tanh, softmax, relu]:
+        print(a_func.__name__)
+        print(a_func(X))
+
+    print("\nACTIVATION DERIVATIVES")
+    for a_func in [d_identity, d_sigmoid, d_tanh, d_softmax, d_relu]:
         print(a_func.__name__)
         print(a_func(X))
 